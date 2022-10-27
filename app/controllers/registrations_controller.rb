19d8ac7dbe7abc458943af5e283111c7c0f14ce7
Martin Afani <afani.dev@gmail.com>
2022-10-27 12:35:29 +0200

    Fix profile update bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   private
     def sign_up_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :role_id)
+      params.require(:user).permit(:email, :first_name, :last_name, :date_of_birth, :phone_number, :address, :gender, :password, :password_confirmation).merge(role_id: 1)
     end
 
     def account_update_params
